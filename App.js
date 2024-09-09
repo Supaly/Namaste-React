d5cd833cb6855285e109e5d282af26efa4eaa738
Supaly <supalygarg5@gmail.com>
2024-09-09 16:27:15 +0530

    part-2

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
 const heading = React.createElement(
     "h1",
-    { id: "heading" },
+    { id: "heading" }, 
     "Hello from React");
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
-// react element is an object that is converted to html that browser undersatnds.
 
 const parent = React.createElement(
     "div",
